47124726d01de050a97558ab2d531d4edd092cb4
Adile Ikhsan Muhammad Al Masyhuri <adileikhsan1902@gmail.com>
2024-10-31 17:11:29 +0700

    Nomor1.cpp

--- QuizLiveCodingPemrogamanDasar/Nomor1.cpp
+++ QuizLiveCodingPemrogamanDasar/Nomor1.cpp
@@ -1,38 +1,20 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-/* 
-Diketahui:
-M = massa
-V = kecepatan
-*/
-
-static void hitungEK(double M, double V) {
-
-    // Menghitung energi kinetik
-
-    double EK = 0.5 * M * ( V * V );
-    cout << "Besar Energi Kinetik: " << EK << " Joule" << endl;
+void hitungEK(double m, double v) {
+	double EK = 0.5 * m * (v * v);
+	cout << "Besar energi kinetik: " << EK << "Joule" << endl;
 }
+int main() {
+	double massa_sepeda, berat_andika, kecepatan,total_massa;
+	cout << "Masukkan massa sepeda Andika (gram): ";
+	cin >> massa_sepeda;
+	cout << "Masukkan berat Andika (Kg): ";
+	cin >> berat_andika;
+	cout << "Masukkan kecepatan (m/s): ";
+	cin >> kecepatan;
 
-void main() {
-    
-    double M_Sepeda_Gram, M_Andika_Kg, V;
-
-    // Input massa dan kecepatan :
-
-    cout << "Masukkan massa sepeda dalam gram: ";
-    cin >> M_Sepeda_Gram;
-    cout << "Masukkan massa Andika dalam kg: ";
-    cin >> M_Andika_Kg;
-    cout << "Masukkan kecepatan dalam m/s: ";
-    cin >> V;
-
-    // Mengubah massa ke Kg :
-    double M_Total = (M_Sepeda_Gram / 1000) + M_Andika_Kg;
-
-    // Hasil fungsi hitungEK :
-    hitungEK(M_Total, V);
-
- 
+	total_massa = (massa_sepeda / 1000) + berat_andika;
+	hitungEK(total_massa, kecepatan);
+	return 0;
 }
